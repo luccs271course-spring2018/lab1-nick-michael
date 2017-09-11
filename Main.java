e33c515df5b59b3cf3c58d73a7e42eec03328c9d
ntaylor5 <31541603+ntaylor5@users.noreply.github.com>
2017-09-10 19:32:30 -0500

    Add files via upload
    This is the fizzBuzz program fulfilling Part 2 of the assignment

---
+++ Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //variables:
+        int input;
+        int n = 1;
+        Scanner kb = new Scanner(System.in);
+
+        //PART 2
+        //This code was written by Nick Taylor, Michael Woelfel, and Miguel Gonzales
+
+        System.out.println("fizzBuzz a number:");
+        input = kb.nextInt();
+        while (input > 0) {
+            if (n % 3 == 0)
+            {
+                System.out.print("Fizz ");
+            }
+            if (n % 5 == 0)
+            {
+                System.out.print("Buzz");
+            }
+            if (n % 3 != 0 && n % 5 != 0)
+            {
+                System.out.print(Integer.toString(n));
+            }
+            System.out.println();
+            n++;
+            input--;
+        }
+    }
+}
